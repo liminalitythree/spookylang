22c92c7620b05a1cc34d65808e103ae612cf12b7
lain <30793687+liminalitythree@users.noreply.github.com>
2020-06-13 18:57:35 -0600

    dum

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod lexer;
+mod parser;
+
+fn main() {
+    let input = "Int.Fuction('fib', args=Int.New('i'), opts=(Int.New('a'), Int.New('b')), body=(
+        (a.isUnset().then(a.set(0))),
+        (b.isUnset().then(b.set(1))),
+        (i.isEqual(0).then(this.exit(a), otherwise=this.exit(this(i.sub(1), b, a.add(b)))))
+    ))
+    ";
+    let mut lexer = lexer::Lexer::new(input.to_string());
+    let tokens = lexer.scan_tokens();
+
+    println!("{:?}", tokens);
+}
